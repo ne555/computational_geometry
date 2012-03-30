aa0d74bec6d0d9f8111a9e77b49ac8302d65a804
wbedrij <wbedrij@unl.edu.ar>
2012-03-30 10:51:56 -0300

    Skeleton

--- convex_hull/geometric_algorithms.cpp
+++ convex_hull/geometric_algorithms.cpp
@@ -5,7 +5,14 @@
 namespace geometry{
 	namespace convex_hull{
 		std::vector<point2d> incremental(std::vector<point2d> points){
+			if(points.size()<3) return std::vector<point2d>();
 			std::sort(points.begin(), points.end());
+			std::list<point2d> hull(points.begin(), points.begin()+3);
+			for(size_t K=3; K<points.size(); ++K){
+				
+			}
+
+			return std::vector<point2d>(hull.begin(), hull.end());
 		}
 	}
 }
